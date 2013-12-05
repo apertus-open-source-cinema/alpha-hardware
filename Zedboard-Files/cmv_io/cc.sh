351e8d7424b771f8d238e42ca9950810dabac28c
Herbert Poetzl <herbert@13thfloor.at>
2013-12-05 13:34:55 +0100

    obsolete, can now be found in alpha-software

--- Zedboard-Files/cmv_io/cc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed '   /^[iI][nN][fF][oO]/		{ s/.*/[34m&[0m/; T }
-	/^[eE][rR][rR][oO][rR]/		{ s/.*/[31m&[0m/; T }
-	/^[mM][eE][sS][sS][aA][gG][eE]/	{ s/.*/[32m&[0m/; T }
-	/^[wW][aA][rR][nN]/		{ s/.*/[33m&[0m/; T }
-	/^CRITICAL/			{ s/.*/[31m&[0m/; T }
-	'
